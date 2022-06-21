4a094eec7cffcdfcdef486b096839eccf112b601
Kuwertzel <73904521+Kuwertzel@users.noreply.github.com>
2022-06-21 22:53:48 +0200

    Optionally pass state attributes to set_state()
    If the parameter "attributes" is specified, it expects a dict of attributes that will overwrite/remove (!) existing attributes of a given state.
    
    Example:
    hass.set_state("input_text.status_text", "online", attributes={"attr1": 1, "attr2": "SomeString"})

--- src/hassapi/client/states.py
+++ src/hassapi/client/states.py
@@ -12,10 +12,12 @@ class StatesClient(BaseClient):
         """Get ``entity_id`` state."""
         return State(**self._get(f"states/{entity_id}"))  # type: ignore
 
-    def set_state(self, entity_id: str, state: HassValueType) -> State:
-        """Set ``entity_id`` state."""
+    def set_state(self, entity_id: str, state: HassValueType, attributes: dict = None) -> State:
+        """Set ``entity_id`` state and optionally attributes."""
+        if attributes:
+            return State(**self._post(f"states/{entity_id}", state=state, attributes=attributes))  # type: ignore
         return State(**self._post(f"states/{entity_id}", state=state))  # type: ignore
-
+    
     def get_states(self) -> StateList:
         """Get states of all entities."""
         return StateList(self._get("states"))
